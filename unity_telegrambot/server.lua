9339cd915ce7ea1542b93310aa3acad4672d015a
Unity-Dev <71393509+Unity-Dev2020@users.noreply.github.com>
2021-04-11 20:47:53 +0200

    server.lua
    | Unity Dev Team |
    Receive logs on your smarthphone with Telegram!
    
    Discord: https://discord.gg/TaMf7YT

---
+++ unity_telegrambot/server.lua
@@ -0,0 +1,20 @@
+-------------- https://discord.gg/TaMf7YT | IF YOU NEED HELP JOIN OUR DISCORD --------------
+
+-- Bot token:  TOKENXXXX:TOKENYYYYYY --
+
+-- chat ID: 29xxxxx --
+
+-- https://api.telegram.org/botTOKENXXXX:TOKENYYYYYY/sendMessage?chat_id=29XXXXX&text= "YOU LOG TEXT" --
+
+RegisterServerEvent("unitydev:telegramlog")
+AddEventHandler("unitydev:telegramlog", function(text) -- You can also add player name, player ID... everything. That's just a demonstrative event
+        
+    PerformHttpRequest("https://api.telegram.org/botTOKENXXXX:TOKENYYYYYY/sendMessage?chat_id=29XXXXX&text=%22..text, 
+    function(err, text, header) end, 
+    'POST')
+        
+    -- IF YOU NEED ANY HELP, JOIN OUR DISCORD: https://discord.gg/TaMf7YT --    
+        
+end)
+-- You can trigger this event where you want, chat logs, bank robbery logs, kill logs and much more --
+-------------- https://discord.gg/TaMf7YT | YOU CAN FIND OTHER RESOURCES ON OUR DISCORD --------------
